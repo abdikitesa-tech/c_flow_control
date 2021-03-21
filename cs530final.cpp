09f78b69e3cb40e5e08fc351b8768e566c1b6d28
abdikitesa-tech <77311891+abdikitesa-tech@users.noreply.github.com>
2021-03-21 18:35:03 +0300

    Update cs530final.cpp
    * an array of table shape
                      table cost
                      finish material --- is added to make final report appear after receiving multiple cost estimation.
    * and also a triangular shape table is added to shape choice option

--- cs530final.cpp
+++ cs530final.cpp
@@ -1,31 +1,33 @@
-//written By Abdi Kitesa
-//Written Date: 7-Mar-2021
-
+//A program that accepts user's choice of table shape, measurement and finishing material , as an input, calculate accordingly and display total cost as an output
 #include <iostream>
 #include <stdlib.h>
 #include <iomanip>
-using namespace std;
+
+using namespace std;;
 int main()
 { cout << fixed << setprecision(2);// money
     char tableShape;
-    float tableLength = 0,tableArea = 0,tableWidth = 0,tableDiameter = 0,tableRadius = 0,tableCost = 0,tableCostSum = 0;
+    float tableLength = 0,tableArea = 0,tableWidth = 0,tableDiameter = 0,tableRadius = 0,tableCost = 0,tableCostSum = 0, tableBase = 0, tableHeight = 0;
+    float arrTableArea[10] = {0}; string shape [10] = {" "}; string material [10] = {" "}; float arrTableCost[10] = {0};
     const float M_PI = 3.14;
     char materialType;
     int ageTotal = 0;
     int num_Of_tableEstm = 0;
     bool materialTypeSel = 0;
+    int tableIndex = 0;
 
     cout << "Welcome to Tables – Yours One Stop Table Shop\n"
-         << "Dawit Lemma – Master Table Builder\n"
+         << "Abdi Kitesa – Master Table Builder\n"
          << "What shape of table do you want to build?\n"
          << "1. Rectangular\n"
          << "2. Square\n"
          << "3. Circular\n"
-         << "4. End\n";
+         << "4. Triangular\n"
+         << "5. End\n";
 
 
     cin >> tableShape;  //accept shape choice
-while (tableShape >'4') // input validation
+while (tableShape >'5') // input validation
 {
     cout <<"Error – Invalid Entry. Please reenter a valid value\n";
         system("cls");
@@ -33,7 +35,8 @@ while (tableShape >'4') // input validation
          << "1. Rectangular\n"
          << "2. Square\n"
          << "3. Circular\n"
-         << "4. End\n";
+         << "4. triangular"
+         << "5. End\n";
 }
 if(tableShape >= '1' && tableShape <= '3')
 {
@@ -43,11 +46,11 @@ if(tableShape >= '1' && tableShape <= '3')
             cin >> tableLength;
                  while (tableLength <= 0) //validation of length input
                 {
-                cout << "Error – Length must be greater than zero. Please reenter a valid value";
-                cin >> tableLength;
+                    cout << "Error – Length must be greater than zero. Please reenter a valid value";
+                    cin >> tableLength;
                 }
                 cout << "Enter the width of the table (in inches): ";
-               cin >> tableWidth;
+                cin >> tableWidth;
                  while (tableWidth <= 0) //validation of width input
                 {
                 cout << "Error – width must be greater than zero. Please reenter a valid value";
@@ -71,10 +74,12 @@ if(tableShape >= '1' && tableShape <= '3')
                 num_Of_tableEstm++;
                 tableCostSum = tableCost + tableCostSum;
 
-                cout << "Output Report\n"
-                     << "The area of the table is" << tableArea << "square inches\n"
-                     << "The table will be made of Laminate\n"
-                     << "The cost of this table is $" << tableCost;
+               tableIndex = tableIndex + 1;
+               arrTableArea[tableIndex] = tableArea;
+               shape [tableIndex] = "Rectangle";
+               material [tableIndex] = "Laminate";
+               arrTableCost [tableIndex] = tableCost;
+
                 }
                 else        //for material choice - oak
                 {
@@ -84,10 +89,11 @@ if(tableShape >= '1' && tableShape <= '3')
                 tableCostSum = tableCost + tableCostSum;
 
 
-                cout << "Output Report\n"
-                     << "The area of the table is" << tableArea << "square inches\n"
-                     << "The table will be made of oak\n"
-                     << "The cost of this table is $" << tableCost;
+               tableIndex = tableIndex + 1;
+               arrTableArea[tableIndex] = tableArea;
+               shape [tableIndex] = "Rectangle";
+               material [tableIndex] = "oak";
+               arrTableCost [tableIndex] = tableCost;
                 }
                     cout <<"What shape of table do you want to build?\n"
                              << "1. Rectangular\n"
@@ -125,10 +131,11 @@ if(tableShape >= '1' && tableShape <= '3')
                 tableCostSum = tableCost + tableCostSum;
 
 
-                cout << "Output Report\n"
-                     << "The area of the table is" << tableArea << "square inches\n"
-                     << "The table will be made of Laminate\n"
-                     << "The cost of this table is $" << tableCost;
+               tableIndex = tableIndex + 1;
+               arrTableArea[tableIndex] = tableArea;
+               shape [tableIndex] = "Rectangle";
+               material [tableIndex] = "Laminate";
+               arrTableCost [tableIndex] = tableCost;
                 }
                 else
                 {
@@ -138,16 +145,18 @@ if(tableShape >= '1' && tableShape <= '3')
                 tableCostSum = tableCost + tableCostSum;
 
 
-                cout << "Output Report\n"
-                     << "The area of the table is" << tableArea << "square inches\n"
-                     << "The table will be made of oak\n"
-                     << "The cost of this table is $" << tableCost;
+                tableIndex = tableIndex + 1;
+               arrTableArea[tableIndex] = tableArea;
+               shape [tableIndex] = "Rectangle";
+               material [tableIndex] = "oak";
+               arrTableCost [tableIndex] = tableCost;
                 }
                     cout <<"What shape of table do you want to build?\n"
                              << "1. Rectangular\n"
                              << "2. Square\n"
                              << "3. Circular\n"
-                             << "4. End\n";
+                             << "4. triangular\n"
+                             << "5. End\n";
                     cin >> tableShape;
 
         }
@@ -173,17 +182,18 @@ if(tableShape >= '1' && tableShape <= '3')
                 }
                 if(materialType == '1')
                 {
-                tableRadius = tableDiameter / 2.0;          //find radius from diameter
+                tableRadius = tableDiameter/2;          //find radius from diameter
                 tableArea = M_PI*tableRadius*tableRadius;
                 tableCost = tableArea * 0.125;
                 num_Of_tableEstm++;                     //counter for number of table estimated
                 tableCostSum = tableCost + tableCostSum;
 
 
-                cout << "Output Report\n"
-                     << "The area of the table is" << tableArea << "square inches\n"
-                     << "The table will be made of Laminate\n"
-                     << "The cost of this table is $" << tableCost;
+               tableIndex = tableIndex + 1;
+               arrTableArea[tableIndex] = tableArea;
+               shape [tableIndex] = "Rectangle";
+               material [tableIndex] = "Laminate";
+               arrTableCost [tableIndex] = tableCost;
                 }
                 else
                 {
@@ -193,33 +203,99 @@ if(tableShape >= '1' && tableShape <= '3')
                 tableCostSum = tableCost + tableCostSum;
 
 
-                cout << "Output Report\n"
-                     << "The area of the table is" << tableArea << "square inches\n"
-                     << "The table will be made of oak\n"
-                     << "The cost of this table is $" << tableCost;
+                tableIndex = tableIndex + 1;
+               arrTableArea[tableIndex] = tableArea;
+               shape [tableIndex] = "Rectangle";
+               material [tableIndex] = "oak";
+               arrTableCost [tableIndex] = tableCost;
                 }
                     cout <<"What shape of table do you want to build?\n"    //request for additional input
                              << "1. Rectangular\n"
                              << "2. Square\n"
                              << "3. Circular\n"
-                             << "4. End\n";
+                             << "4. triangular\n"
+                             << "5. End\n";
+
                     cin >> tableShape;
 
         }
+ if(tableShape == '4')   // for table choice rectangle
+        {   cout << "Enter the base of the table (in inches): ";
+            cin >> tableBase;
+                 while (tableBase <= 0) //validation of length input
+                {
+                    cout << "Error – Length must be greater than zero. Please reenter a valid value";
+                    cin >> tableBase;
+                }
+                cout << "Enter the height of the table (in inches): ";
+                cin >> tableHeight;
+                 while (tableHeight <= 0) //validation of width input
+                {
+                cout << "Error – width must be greater than zero. Please reenter a valid value";
+                cin >> tableWidth;
+                }
+                cout << "What type of material do you want to use?\n"
+                     << "1.	Laminate ($0.125 per square inch)\n"
+                     << "2.	Oak ($0.25 per square inch)\n";
+                cin >> materialType;
 
+                while (materialType <= '0' && materialType > '2')
+                {
+                cout << "Error – type of material must be either Laminated or Oak. Please reenter a valid value";
+                cin >> materialType;
+                }
+
+                if(materialType == '1') //for material choice - laminate
+                {
+                tableArea = 1/2 * tableBase * tableHeight;
+                tableCost = tableArea * 0.125;
+                num_Of_tableEstm++;
+                tableCostSum = tableCost + tableCostSum;
+
+                 tableIndex = tableIndex + 1;
+               arrTableArea[tableIndex] = tableArea;
+               shape [tableIndex] = "Rectangle";
+               material [tableIndex] = "laminate";
+               arrTableCost [tableIndex] = tableCost;
+                }
+                else        //for material choice - oak
+                {
+                tableArea = 1/2 * tableBase * tableHeight;
+                tableCost = tableArea * 0.25;
+                num_Of_tableEstm++;
+                tableCostSum = tableCost + tableCostSum;
+
+
+                 tableIndex = tableIndex + 1;
+               arrTableArea[tableIndex] = tableArea;
+               shape [tableIndex] = "Rectangle";
+               material [tableIndex] = "oak";
+               arrTableCost [tableIndex] = tableCost;
+                }
+                    cout <<"What shape of table do you want to build?\n"
+                             << "1. Rectangular\n"
+                             << "2. Square\n"
+                             << "3. Circular\n"
+                             << "4. triangular\n"
+                             << "5. End\n";
+                    cin >> tableShape;
+
+        }
         }
     while (tableShape > '0' && tableShape <= '3' && tableShape != '4');
 
 }
-if (tableShape == '4')      
+if (tableShape == '5')
     {  if(num_Of_tableEstm == 0)        //if user leave without estimation request
-        cout<<"no estimation requested";
+        cout<<"no estimation today!";
         else
             {
-        cout << "number of table estimation: "<<num_Of_tableEstm<<"\n"
-             << "total cost of tables" << tableCostSum<<"\n"
-             << "*** Good Bye ***";
-
+            for (int i = 1; i <= tableIndex; i++ )
+                {   cout <<"Table" << i << endl<<endl
+                         <<"\t"<<"The area of " << shape [i] << "table is " << arrTableArea[i] <<"square inch"<<endl
+                         <<"\t"<<"The table will be made of" << material [i] << endl
+                         <<"\t"<<"the cost of the table is " << arrTableCost [i] << "Birr"<<endl;
+                }
             }
     }
 
